7f97d705daf78d21c7eca6e44470aecf35a7f625
Ole Peter Smith <ole.ufg@gmail.com>
2016-11-29 16:35:28 -0200

    oo

---
+++ Siolymp/.DB.php
@@ -0,0 +1,13 @@
+array
+(
+   'ServType' => 'mysql',
+   'Type' => "PDO",
+   'Host' => '127.0.0.1',
+   'DB' => 'siolimp',
+   'User' => 'siolimp',
+   'Password' => 'your-password',
+   
+   'Mod' => TRUE,
+   'Debug' => 2,
+   'MailDebug' => FALSE,
+);
